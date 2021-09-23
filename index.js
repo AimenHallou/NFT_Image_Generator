07be528720410e89478f58cb14a98fa58ff26295
Aimen <hall0408@algonquinlive.com>
2021-09-23 15:47:03 -0400

    Changes to performance and update on drawing

--- index.js
+++ index.js
@@ -1,31 +1,163 @@
 const fs = require("fs");
-const {createCanvas, loadImage } = require("canvas");
+const myArgs = process.argv.slice(2);
+const {
+    createCanvas,
+    loadImage
+} = require("canvas");
+const {
+    layers,
+    width,
+    height
+} = require("./input/config.js");
 
 const console = require("console");
 //Size of the .png
-const canvas = createCanvas(1000,1000);
+const canvas = createCanvas(1000, 1000);
 const ctx = canvas.getContext("2d");
 
-const {layers, width, height} = require("./input/config.js");
 //Select how many verisons
-const edition = 1;
+const editionSize = myArgs.length > 0 ? Number(myArgs[0]) : 1;
+
+var metaDataList = [];
+var attributesList = [];
+
+var dnaList = [];
+
+
 //Once you selected the layers, you export it in the output section
-const saveLayer = (_canvas, _edition)=> {
-    fs.writeFileSync(`./output/./${_edition}.png`,_canvas.toBuffer("image/png"));
+const saveImage = (_editionCount) => {
+    fs.writeFileSync(`./output/./${_editionCount}.png`, canvas.toBuffer("image/png"));
 }
-//Go through the layers and draw each of them
-const drawLayer = async(_layer, _edition) => {
-    //Pick a random element from the list of possible sections
-    let element = _layer.elements[Math.floor(Math.random()* _layer.elements.length)];
-    const image = await loadImage(`${_layer.location}${element.fileName}`);
-    ctx.drawImage(image,_layer.position.x,_layer.position.y,_layer.size.width,_layer.size.height);
-    console.log(`I created the ${_layer.name} layer, and choose element ${element.name}`);
-    saveLayer(canvas, _edition);
+
+//Able to add the adition to the final product  
+const signImage = (_sig) =>{
+    ctx.fillStyle = "#000000";
+    ctx.font = "bold 30pt Courier";
+    ctx.textBaseline = "top";
+    ctx.textAlign = "left";
+    ctx.fillText(_sig, 40 , 40);
 }
-//Make all the different editions
-for(let i = 1; i <= edition; i++){
-    layers.forEach(layer => {
-        drawLayer(layer, i);
+
+//Creates a random color to be draw
+const genColor = () =>{
+    let hue = Math.floor(Math.random() *360);
+    let pastel = `hsl(${hue},100%, 85%)`;
+    return pastel;
+}
+//Allows the user to draw a random background
+const drawBackground = () =>{
+    ctx.fillStyle = genColor();
+    ctx.fillrect(0,0,width,height);
+
+}
+
+//Add the metadata to the file
+const addMetadata = (_dna, _edition) => {
+    let dateTime = Date.now();
+    let tempMetadata = {
+        dna: _dna,
+        edition: _edition,
+        date: dateTime,
+        attributes: attributesList,
+    };
+    metaDataList.push(tempMetadata);
+    attributesList = [];
+};
+
+//Add attributes to each element
+const addAttributes = (_element) => {
+    let selectedElement = _element.layer.selectedElement;
+    attributesList.push({
+        name: selectedElement.name,
+        rarity: selectedElement.rarity
     });
-    console.log("Creating edition "+ i);
+};
+
+//Go through the layers and draw each of them
+const loadLayerImg = async (_layer) => {
+    return new Promise(async (resolve) => {
+        const image = await loadImage(`${_layer.location}${_layer.selectedElement.fileName}`);
+        resolve({
+            layer: _layer,
+            loadedImage: image
+        });
+    });
 }
+
+//Able to draw every layer available
+const drawElement = (_element) => {
+    ctx.drawImage(_element.loadedImage,
+        _element.layer.position.x,
+        _element.layer.position.y,
+        _element.layer.size.width,
+        _element.layer.size.height
+    );
+    addAttributes(_element);
+};
+
+//Create a dna to follow each image
+const constructLayerToDna = (_dna, _layers) => {
+    let DnaSegment = _dna.toString().match(/.{1,2}/g);
+    let mappedDnaToLayers = _layers.map((layer) => {
+        let selectedElement = layer.elements[parseInt(DnaSegment) % layer.elements.length];
+        return {
+            location: layer.location,
+            position: layer.position,
+            size: layer.size,
+            selectedElement: selectedElement,
+        };
+    });
+    return mappedDnaToLayers;
+};
+
+//Make sure each dna is unique to the other ones
+const isDnaUnique = (_DnaList = [], _dna) => {
+    let foundDna = dnaList.find((i) => i === _dna)
+    return foundDna == undefined ? true : false;
+}
+
+//Create a random dna code that follows each image 
+const createDna = (_len) => {
+    let randNum = Math.floor(Number(`1e${_len}`) + Math.random() * Number(`9e${_len}`));
+    return randNum;
+};
+
+//Write to the meta data file 
+const writeMetaData = (_data) => {
+    fs.writeFileSync("./output/_metadata.json", _data);
+
+}
+
+//Make all the different editions
+const startCreating = async () => {
+    writeMetaData("");
+    let editionCount = 1;
+    while (editionCount <= editionSize) {
+        console.log(`RandomNum ${createDna(layers.length*2-1)}`);
+        let newDna = createDna(layers.length * 2 - 1);
+        if (isDnaUnique(dnaList, newDna)) {
+            let results = constructLayerToDna(newDna, layers);
+            let loadedElements = [];
+            results.forEach(layer => {
+                loadedElements.push(loadLayerImg(layer));
+            });
+            await Promise.all(loadedElements).then(elementArry => {
+                drawBackground();
+                elementArry.forEach(element => {
+                    drawElement(element);
+                })
+                signImage(`#${editionCount}`);
+                saveImage(editionCount);
+                addMetadata(newDna, editionCount);
+                console.log(`Created edition ${editionCount} with dna ${newDna}`);
+            });
+            // console.log("Creating edition " + i);
+            dnaList.push(newDna);
+            editionCount++;
+        } else {
+            console.log("DNA exists!");
+        }
+    }
+    writeMetaData(JSON.stringify(metaDataList));
+};
+startCreating();
--- input/config.js
+++ input/config.js
@@ -4,118 +4,163 @@ const width = 1000;
 const height = 1000;
 console.log(__dirname);
 
-
 //Different rarity which can be selected from here
-const rarity = [
-    {key: "", val: "original"},
-    {key: "_r", val: "rare"},
-    {key: "_sr", val: "super rare"},
-    {key: "_s", val: "only one"},
+const rarity = [{
+    key: "",
+    val: "original"
+  },
+  {
+    key: "_r",
+    val: "rare"
+  },
+  {
+    key: "_sr",
+    val: "super rare"
+  },
+  {
+    key: "_s",
+    val: "only one"
+  },
 ];
 
 //Custom rarity to each png file
 const addRarity = _str => {
-    let itemRarity;
-  
-    rarity.forEach((r) => {
-      if (_str.includes(r.key)) {
-        itemRarity = r.val;
-      }
-    });
-    return itemRarity;
-  };
+  let itemRarity;
+
+  rarity.forEach((r) => {
+    if (_str.includes(r.key)) {
+      itemRarity = r.val;
+    }
+  });
+  return itemRarity;
+};
 
 
 //Remove .png and the rarity from it 
 const cleanName = _str => {
-    let name = _str.slice(0, -4);
-    rarity.forEach((r) => {
-      name = name.replace(r.key, "");
-    });
-    return name;
-  };
+  let name = _str.slice(0, -4);
+  rarity.forEach((r) => {
+    name = name.replace(r.key, "");
+  });
+  return name;
+};
 
 //Each element has a id, name, original file name and rarity
 const getElements = path => {
-    return fs
-      .readdirSync(path)
-      .filter((item) => !/(^|\/)\.[^\/\.]/g.test(item))
-      .map((i, index) => {
-        return {
-          id: index+1,
-          name: cleanName(i),
-          fileName: i,
-          rarity: addRarity(i)
-        };
-      });
-  };
+  return fs
+    .readdirSync(path)
+    .filter((item) => !/(^|\/)\.[^\/\.]/g.test(item))
+    .map((i, index) => {
+      return {
+        id: index + 1,
+        name: cleanName(i),
+        fileName: i,
+        rarity: addRarity(i)
+      };
+    });
+};
 //Each layer on the project
-const layers = [
-    {
-    id: 1,
-    name: "background",
+const layers = [{
     location: `${dir}/background/`,
     elements: getElements(`${dir}/background/`),
-    position: {x:0 ,y:0},
-    size:{ width:width, height:height}
- },
- {
-    id: 2,
-    name: "eyeball",
+    position: {
+      x: 0,
+      y: 0
+    },
+    size: {
+      width: width,
+      height: height
+    }
+  },
+  {
     location: `${dir}/Eyeball/`,
     elements: getElements(`${dir}/Eyeball/`),
-    position: {x:0 ,y:0},
-    size:{ width:width, height:height}
-},
-{
-    id: 3,
-    name: "eye color",
+    position: {
+      x: 0,
+      y: 0
+    },
+    size: {
+      width: width,
+      height: height
+    }
+  },
+  {
     location: `${dir}/Eye Color/`,
     elements: getElements(`${dir}/Eye Color/`),
-    position: {x:0 ,y:0},
-    size:{ width:width, height:height}
-},
-{
-    id: 4,
-    name: "iris",
+    position: {
+      x: 0,
+      y: 0
+    },
+    size: {
+      width: width,
+      height: height
+    }
+  },
+  {
     location: `${dir}/Iris/`,
     elements: getElements(`${dir}/Iris/`),
-    position: {x:0 ,y:0},
-    size:{ width:width, height:height }
-},
-{
-    id: 5,
-    name: "shine",
+    position: {
+      x: 0,
+      y: 0
+    },
+    size: {
+      width: width,
+      height: height
+    }
+  },
+  {
     location: `${dir}/Shine/`,
     elements: getElements(`${dir}/Shine/`),
-    position: {x:0 ,y:0},
-    size:{ width:width, height:height}
-},
-{
-    id: 6,
-    name: "bottom lid",
+    position: {
+      x: 0,
+      y: 0
+    },
+    size: {
+      width: width,
+      height: height
+    }
+  },
+  {
     location: `${dir}/Bottom lid/`,
     elements: getElements(`${dir}/Bottom lid/`),
-    position: {x:0 ,y:0},
-    size:{ width:width, height:height}
-},
-{
-    id: 7,
-    name: "top lid",
+    position: {
+      x: 0,
+      y: 0
+    },
+    size: {
+      width: width,
+      height: height
+    }
+  },
+  {
     location: `${dir}/Top lid/`,
     elements: getElements(`${dir}/Top lid/`),
-    position: {x:0 ,y:0},
-    size:{ width:width, height:height}
- },
- {
-    id: 8,
-    name: "goo",
+    position: {
+      x: 0,
+      y: 0
+    },
+    size: {
+      width: width,
+      height: height
+    }
+  },
+  {
     location: `${dir}/Goo/`,
     elements: getElements(`${dir}/Goo/`),
-    position: {x:0 ,y:0},
-    size:{ width:width, height:height}
-},
+    position: {
+      x: 0,
+      y: 0
+    },
+    size: {
+      width: width,
+      height: height
+    }
+  },
 ];
 
 //Bring layers, width and height to the index file
-module.exports = {layers, width, height}
+module.exports = {
+  layers,
+  width,
+  height
+}
